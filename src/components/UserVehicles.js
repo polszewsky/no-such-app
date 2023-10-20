f4b75b9b5954615b20081d6c2e77b399736996ca
szymon-rogus <sszymek98@gmail.com>
2023-10-21 00:40:46 +0200

    JSONS

---
+++ src/components/UserVehicles.js
@@ -0,0 +1,94 @@
+const cars = {
+    "cars": [
+      {
+        "brand": "Audi",
+        "model": "A4",
+        "year": 2014,
+        "engine": "benzynowy",
+        "fuelUsage": 6.5,
+        "CO2": 0.15,
+        "electricityUsage": 0
+      },
+      {
+        "brand": "Volkswagen",
+        "model": "Golf",
+        "year": 2018,
+        "engine": "diesel",
+        "fuelUsage": 4.0,
+        "CO2": 0.105,
+        "electricityUsage": 0
+      },
+      {
+        "brand": "Toyota",
+        "model": "Prius",
+        "year": 2023,
+        "engine": "hybrydowy",
+        "fuelUsage": 3.5,
+        "CO2": 0.08,
+        "electricityUsage": 0
+      },
+      {
+        "brand": "Tesla",
+        "model": "Model 3",
+        "year": "N/A",
+        "engine": "elektryczny",
+        "fuelUsage": 0.0,
+        "electricityUsage": 15
+      }
+    ]
+  }
+
+  const planes = {
+    "samoloty_pasażerskie": [
+      {
+        "model": "Boeing 747-400",
+        "rozmiar": "duży",
+        "ilość_pasażerów": 416,
+        "obłożenie_miejsc": "80%",
+        "średnia_liczba_pasażerów": 333,
+        "dystans": "5556 km",
+        "zużycie_paliwa_100km": "32.2 g na pasażera na km",
+        "emisjaCO2_100km": "101 g na pasażera na km",
+        "prędkość_przelotowa": "910 km/h",
+        "emisjaCO2_godzina": "92 kg CO2 na pasażera na godzinę"
+      },
+      {
+        "model": "Airbus A320",
+        "rozmiar": "średni",
+        "ilość_pasażerów": 150,
+        "obłożenie_miejsc": "85%",
+        "średnia_liczba_pasażerów": 127,
+        "dystans": "3200 km",
+        "zużycie_paliwa_100km": "3.4 l/100km/pasażer",
+        "emisjaCO2_100km": "9.1 kg CO2/100km/pasażer",
+        "prędkość_przelotowa": "840 km/h",
+        "emisjaCO2_godzina": "35 kg CO2 na pasażera na godzinę"
+      },
+      {
+        "model": "Boeing 787 Dreamliner",
+        "rozmiar": "średni",
+        "ilość_pasażerów": 290,
+        "obłożenie_miejsc": "75%",
+        "średnia_liczba_pasażerów": 218,
+        "dystans": "8000 km",
+        "zużycie_paliwa_100km": "2.9 l/100km/pasażer",
+        "emisjaCO2_100km": "7.8 kg CO2/100km/pasażer",
+        "prędkość_przelotowa": "903 km/h",
+        "emisjaCO2_godzina": "30 kg CO2 na pasażera na godzinę"
+      },
+      {
+        "model": "Embraer E175",
+        "rozmiar": "mały",
+        "ilość_pasażerów": 76,
+        "obłożenie_miejsc": "70%",
+        "średnia_liczba_pasażerów": 53,
+        "dystans": "2500 km",
+        "zużycie_paliwa_100km": "4.7 l/100km/pasażer",
+        "emisjaCO2_100km": "12.6 kg CO2/100km/pasażer",
+        "prędkość_przelotowa": "840 km/h",
+        "emisjaCO2_godzina": "53 kg CO2 na pasażera na godzinę"
+      }
+    ]
+  }
+
+  export default cars;
