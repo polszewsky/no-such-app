cefffb3a9cac57de9d8739ab936a16a840f94c55
polszewsky <32812297+polszewsky@users.noreply.github.com>
2023-10-20 07:17:07 +0200

    Basic dependency config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
-function App() {
+/**
+ * This should be our main page where we login and create router for whole app
+ * @returns
+ */
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>#SustainableFuture</code> NoSuchApp
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import { Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../reducers/counterReducer";
+
+export default function Home() {
+  const dispatch = useDispatch();
+
+  const { value } = useSelector((state) => state.counter);
+
+  return (
+    <div>
+      Home
+      <p>Counter: {value ?? 0}</p>
+      <p>
+        <Button variant="contained" onClick={() => dispatch(increment())}>
+          +1
+        </Button>
+        <Button variant="contained" onClick={() => dispatch(decrement())}>
+          -1
+        </Button>
+      </p>
+    </div>
+  );
+}
