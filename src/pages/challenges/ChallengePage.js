73b3c71cf76390db62511cbf8b94e727438baa90
polszewsky <32812297+polszewsky@users.noreply.github.com>
2023-10-21 23:16:14 +0200

    Challenges - Part 1

---
+++ src/pages/challenges/ChallengePage.js
@@ -0,0 +1,58 @@
+import { Badge, Grid, Paper, Typography, Button } from "@mui/material";
+import React, { Fragment } from "react";
+import TitleReturnBar from "../../components/TitleReturnBar";
+
+export default function ChallengePage() {
+  return (
+    <Fragment>
+      <TitleReturnBar site="Challenges" />
+
+      <Grid container direction="column">
+        <Grid container item direction="row" sx={{ marginTop: "1rem" }} xs={12}>
+          <Grid item xs={12} sx={{}}>
+            <Paper
+              elevation={3}
+              sx={{ padding: "1rem", background: "#2f6271" }}
+            >
+              <Grid container item direction="row">
+                <Grid item xs={10}>
+                  <Typography variant="h5">EcoDrive</Typography>
+                </Grid>
+                <Badge
+                  badgeContent={"50pkt"}
+                  color="primary"
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  sx={{
+                    "& .MuiBadge-badge": {
+                      fontSize: 21,
+                      height: 50,
+                      minWidth: 90,
+                      padding: "0.5rem",
+                    },
+                  }}
+                >
+                  <Grid item xs={2}>
+                    &nbsp;
+                  </Grid>
+                </Badge>
+              </Grid>
+              <Grid container item direction="row">
+                <Grid item xs={12}>
+                  <Typography>
+                    Reduce your car's combustion by 10% over the next 100km.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button>Take up!</Button>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
+}
