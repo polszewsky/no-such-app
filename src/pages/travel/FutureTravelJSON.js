ba5e7da82b0750641dcd809cf79c8ab2d7006f90
szymon-rogus <sszymek98@gmail.com>
2023-10-21 11:34:37 +0200

    Past and future travels

---
+++ src/pages/travel/FutureTravelJSON.js
@@ -0,0 +1,41 @@
+const futureTravels = {
+    "travels": [
+        {
+            "from": "Prague",
+            "to": "Łódź",
+            "CO2": 98,
+            "type": "train",
+            "date": "05.11.2023"
+        },
+        {
+            "from": "Madrid",
+            "to": "Sevilla",
+            "CO2": 5,
+            "type": "bike",
+            "date": "29.11.2023"
+        },
+        {
+            "from": "Talinn",
+            "to": "Kaunus",
+            "CO2": 12,
+            "type": "bike",
+            "date": "09.01.2024"
+        },
+        {
+            "from": "New York",
+            "to": "Rijad",
+            "CO2": 8809,
+            "type": "plane",
+            "date": "22.03.2024"
+        },
+        {
+            "from": "Essen",
+            "to": "Monachium",
+            "CO2": 358,
+            "type": "car",
+            "date": "15.06.2024"
+        }
+    ]
+}
+
+export default futureTravels
---
+++ src/pages/travel/PastTravelJSON.js
@@ -0,0 +1,41 @@
+const pastTravels = {
+    "travels": [
+        {
+            "from": "Cracow",
+            "to": "Warsaw",
+            "CO2": 122,
+            "type": "train",
+            "date": "12.12.2022"
+        },
+        {
+            "from": "Berlin",
+            "to": "London",
+            "CO2": 437,
+            "type": "plane",
+            "date": "12.10.2022"
+        },
+        {
+            "from": "Lisbon",
+            "to": "Kyiv",
+            "CO2": 1579,
+            "type": "plane",
+            "date": "01.01.2023"
+        },
+        {
+            "from": "Cairo",
+            "to": "Rome",
+            "CO2": 1408,
+            "type": "plane",
+            "date": "13.05.2023"
+        },
+        {
+            "from": "Hamburg",
+            "to": "Hague",
+            "CO2": 12,
+            "type": "bike",
+            "date": "21.08.2023"
+        }
+    ]
+}
+
+export default pastTravels
