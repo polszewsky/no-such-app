e0ccfd3e2a656abebb1421633022f16c8f092176
polszewsky <32812297+polszewsky@users.noreply.github.com>
2023-10-22 09:40:46 +0200

    Fix for community

--- src/pages/community/CommunityPage.js
+++ src/pages/community/CommunityPage.js
@@ -5,9 +5,9 @@ import Typography from "@mui/material/Typography";
 import Fab from "@mui/material/Fab";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import { community } from "../../components/Community";
-import Co2Icon from '@mui/icons-material/Co2';
-import TokenIcon from '@mui/icons-material/Token';
-import PercentIcon from '@mui/icons-material/Percent';
+import Co2Icon from "@mui/icons-material/Co2";
+import TokenIcon from "@mui/icons-material/Token";
+import PercentIcon from "@mui/icons-material/Percent";
 
 export default function CommunityPage() {
   return (
@@ -17,69 +17,6 @@ export default function CommunityPage() {
       alignItems="center"
       justifyContent="center"
     >
-<<<<<<< HEAD
-      <Grid item xs={12}>
-        <CardMedia
-          component="img"
-          height="128"
-          width="128"
-          image={image}
-          alt="Paella dish"
-          sx={{ objectFit: "contain", paddingTop: "1rem" }}
-        />
-      </Grid>
-      <Grid item container spacint={2} marginTop={2} >
-        <Grid item xs={12}>
-        <Card elevation={3} sx={{ minWidth: 300, marginTop: 2, bgcolor: "#42a5f5" }}>
-              <CardContent>
-                <Grid
-                  container
-                  item
-                  direction="row"
-                  justifyContent="space-around"
-                  alignItems="center"
-                >
-                  <Typography
-                    sx={{ fontSize: 25 }}
-                    color="text.secondary"
-                    gutterBottom
-                  >
-                    Your statistics
-                  </Typography>
-                </Grid>
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="space-around"
-                  alignItems="center"
-                >
-                  <Grid item>
-                    <Typography variant="h6"><b>CO2</b></Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6"><b>Tokens</b></Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6"><b>Better than</b></Typography>
-                  </Grid>
-                </Grid>
-               <hr/>
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="space-around"
-                  alignItems="center"
-                >
-                  <Grid item>
-                    <Typography sx={{ fontSize: 20 }} >157</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography sx={{ fontSize: 20 }}>5069</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography sx={{ fontSize: 20 }}>72%</Typography>
-                  </Grid>
-=======
       <Grid item container marginTop={2} direction="row">
         <Grid item xs={12}>
           <Card
@@ -124,7 +61,6 @@ export default function CommunityPage() {
                   <Typography variant="h6">
                     <b>Better than</b>
                   </Typography>
->>>>>>> master
                 </Grid>
               </Grid>
               <hr />
@@ -183,7 +119,9 @@ export default function CommunityPage() {
                     color="text.secondary"
                     gutterBottom
                   >
-                    <b>{element.name} {element.surname}</b>
+                    <b>
+                      {element.name} {element.surname}
+                    </b>
                   </Typography>
                 </Grid>
                 <hr />
@@ -198,10 +136,14 @@ export default function CommunityPage() {
                     <Typography sx={{ fontSize: 20 }}>{element.CO2}</Typography>
                   </Grid>
                   <Grid item>
-                    <Typography sx={{ fontSize: 20 }}>{element.points}</Typography>
+                    <Typography sx={{ fontSize: 20 }}>
+                      {element.points}
+                    </Typography>
                   </Grid>
                   <Grid item>
-                    <Typography sx={{ fontSize: 20 }}>{element.betterThan}</Typography>
+                    <Typography sx={{ fontSize: 20 }}>
+                      {element.betterThan}
+                    </Typography>
                   </Grid>
                 </Grid>
               </CardContent>
