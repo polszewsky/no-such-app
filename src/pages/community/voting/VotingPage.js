cdcdb4f17a0efb24f3f989816267c461d823dadf
polszewsky <32812297+polszewsky@users.noreply.github.com>
2023-10-22 11:51:47 +0200

    Update VotingPage.js

--- src/pages/community/voting/VotingPage.js
+++ src/pages/community/voting/VotingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Fragment } from "react";
 import TitleReturnBar from "../../../components/TitleReturnBar";
-import { CardMedia, Grid } from "@mui/material";
+import { CardMedia, Grid, Typography } from "@mui/material";
 import voting from "../../../components/img/voting.jpg";
 
 export default function VotingPage() {
@@ -11,6 +11,10 @@ export default function VotingPage() {
 
       <Grid container direct="column" spacing={3}>
         <Grid container item direction="row" sx={{ marginTop: "1rem" }}>
+          <Grid item>
+            <Typography variant="h2">Tree of The Year</Typography>
+          </Grid>
+
           <Grid item xs={12}>
             <CardMedia component="img" image={voting} alt="green iguana" />
           </Grid>
